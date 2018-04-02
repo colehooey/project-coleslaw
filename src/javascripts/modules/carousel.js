7ab6466d598a7e5582499f47ae2315cc44955226
Jordan LaPointe <hello@jordanlapointe.com>
2018-04-02 17:25:44 -0500

    fix mobile carousel bug

--- src/javascripts/modules/carousel.js
+++ src/javascripts/modules/carousel.js
@@ -3,33 +3,39 @@
 const wrapperClass = 'js-carousel'
 const itemClass = 'js-carousel-item'
 const itemHiddenClass = 'is-hidden'
+const animationDuration = 400 // Milliseconds
 const groupSize = window.innerWidth < 600 ? 4 : 8
 const updateDelay = 10 // Seconds
 
 // ## Methods
 const hideItems = (itemElements, callback) => {
-  const removeItem = item => {
-    item.style.display = 'none'
+  var callbackWasRun = false
+  const runCallback = () => {
+    if (callbackWasRun) return
     callback()
+    callbackWasRun = true
+  }
+  const hideItem = (item) => {
+    item.style.display = 'none'
+    runCallback()
   }
 
-  itemElements.forEach(item => {
+  itemElements.forEach((item, index) => {
     item.classList.add(itemHiddenClass)
-    item.addEventListener('transitionend', () => {
-      removeItem(item)
-    }, { once: true })
-  })
-}
-
-const showItems = itemElements => {
-  itemElements.forEach(item => {
-    item.style.display = 'block'
     window.setTimeout(
-      () => item.classList.remove(itemHiddenClass),
-      60
+      () => hideItem(item),
+      animationDuration
     )
   })
 }
+
+const showItems = itemElements =>
+  itemElements.forEach(item => {
+    item.style.display = 'block'
+    window.requestAnimationFrame(() =>
+      item.classList.remove(itemHiddenClass)
+    )
+  })
 
 const scheduleUpdate = (carouselItemElements, groupNumber = 0) =>
   window.setTimeout(
@@ -50,14 +56,13 @@ const updateDisplay = (carouselItemElements, currentGroup = 0) => {
   const isLastGroup = currentGroup >= groupCount - 1
   const nextGroup = isLastGroup ? 0 : currentGroup + 1
   const isItemInCurrentGroup = index => index >= groupStart && index < groupEnd
-
-  currentGroup = isLastGroup ? 0 : currentGroup
-
   const currentGroupItems = Array.from(carouselItemElements).filter((item, index) => isItemInCurrentGroup(index))
   const otherGroupItems = Array.from(carouselItemElements).filter((item, index) => !isItemInCurrentGroup(index))
 
-  hideItems(currentGroupItems, () => showItems(otherGroupItems))
-  scheduleUpdate(carouselItemElements, nextGroup)
+  hideItems(otherGroupItems, () => {
+    showItems(currentGroupItems)
+    scheduleUpdate(carouselItemElements, nextGroup)
+  })
 }
 
 // ## Bind events
